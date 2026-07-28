6e81baffbfd09171b4901fd9bd13e38788f18d4b
binkim00 <k0b0301@naver.com>
2026-07-28 16:57:27 +0900

    fix(infra): 운영 배포 env 경로 수정

--- infra/prod/scripts/deploy.sh
+++ infra/prod/scripts/deploy.sh
@@ -3,7 +3,7 @@ set -e
 
 PROJECT_ROOT="/home/ubuntu/docker/project/S15P11E106"
 COMPOSE_FILE="$PROJECT_ROOT/infra/prod/docker-compose.prod.yml"
-ENV_FILE="$PROJECT_ROOT/infra/prod/.env"
+ENV_FILE="/home/ubuntu/docker/project/.env"
 
 cd "$PROJECT_ROOT"
 
